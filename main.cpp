8d2a3ea70b2e888557544169605c5c16a35cc4fe
serpentar <oussama.boukyoud@gmail.com>
2016-03-14 09:36:02 +0000

    variable x in main file

--- main.cpp
+++ main.cpp
@@ -15,9 +15,10 @@ int main(int argc, char *argv[])
 int h[3][3];
 long x;
 // load an image 
-IplImage* img1=cvLoadImage("C:\\images\\phare.png",CV_LOAD_IMAGE_UNCHANGED); 
-Mat img=imread("C:\\images\\phare.png ",CV_LOAD_IMAGE_UNCHANGED); 
+IplImage* img1=cvLoadImage("/Users/serpentar/Desktop/traitement_image/K/lena.png",CV_LOAD_IMAGE_UNCHANGED); 
+Mat img=imread("/Users/serpentar/Desktop/traitement_image/K/lena.png ",CV_LOAD_IMAGE_UNCHANGED); 
 if(img.empty()){ 
 printf("Could not load image file\n"); 
 exit(0); 
-} 
+}
+}
